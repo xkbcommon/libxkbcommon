522be162b274897f9cb9d682312b150b27daa6ff
Ran Benita <ran234@gmail.com>
2012-04-10 22:04:05 +0300

    Use LT_INIT instead of deprecated AC_PROG_LIBTOOL
    And add some warnings to the autoreconf step, for the developers to
    see.
    
    Signed-off-by: Ran Benita <ran234@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd "$srcdir"
 
-autoreconf -v --install || exit 1
+autoreconf --verbose --install --symlink --warnings=all || exit 1
 cd "$ORIGDIR" || exit $?
 
 exec "$srcdir/configure" --enable-maintainer-mode "$@"
