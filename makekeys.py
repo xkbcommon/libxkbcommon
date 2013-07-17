870913c234f82872f1c5816af39c09476e6969cf
Jasper St. Pierre <jstpierre@mecheye.net>
2013-07-17 17:44:07 -0400

    makekeys: Remove some semis
    Python doesn't have these

--- makekeys.py
+++ makekeys.py
@@ -11,13 +11,13 @@ print('''struct name_keysym {
     xkb_keysym_t keysym;
 };\n''')
 
-print('static const struct name_keysym name_to_keysym[] = {');
+print('static const struct name_keysym name_to_keysym[] = {')
 for (name, _) in sorted(entries, key=lambda e: e[0].lower()):
     print('    {{ "{name}", XKB_KEY_{name} }},'.format(name=name))
 print('};\n')
 
 # *.sort() is stable so we always get the first keysym for duplicate
-print('static const struct name_keysym keysym_to_name[] = {');
+print('static const struct name_keysym keysym_to_name[] = {')
 for (name, _) in (next(g[1]) for g in itertools.groupby(sorted(entries, key=lambda e: e[1]), key=lambda e: e[1])):
     print('    {{ "{name}", XKB_KEY_{name} }},'.format(name=name))
 print('};')
