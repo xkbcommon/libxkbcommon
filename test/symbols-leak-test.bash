fa1b4543282413598e6f33f58903bfd55f0703e9
Ran Benita <ran234@gmail.com>
2016-03-13 20:56:58 +0200

    test: add a test that all symbol version file is updated
    It is easy to forget to update these files when adding new symbols.
    
    Stolen with slight changes from libinput (commit by Marek Chalupa):
    https://cgit.freedesktop.org/wayland/libinput/commit/?id=a9f216ab47ea2f643f20ed741b741a2b5766eba3
    
    Signed-off-by: Ran Benita <ran234@gmail.com>

---
+++ test/symbols-leak-test.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Check that all exported symbols are specified in the symbol
+# version scripts.  If this fails, please update the appropriate
+# (adding new version nodes when needed).
+
+# xkbcommon symbols
+diff -a -u \
+    <(cat "$top_srcdir"/xkbcommon.map | \
+        grep '^\s\+xkb_.*' | \
+        sed -e 's/^\s\+\(.*\);/\1/' | sort) \
+    <(cat "$top_srcdir"/src/{,xkbcomp,compose}/*.c | \
+        grep XKB_EXPORT -A 1 | grep '^xkb_.*' | \
+        sed -e 's/(.*//' | sort)
+
+# xkbcommon-x11 symbols
+diff -a -u \
+    <(cat "$top_srcdir"/xkbcommon-x11.map | \
+        grep '^\s\+xkb_.*' | \
+        sed -e 's/^\s\+\(.*\);/\1/' | sort) \
+    <(cat "$top_srcdir"/src/x11/*.c | \
+        grep XKB_EXPORT -A 1 | grep '^xkb_.*' | \
+        sed -e 's/(.*//' | sort)
