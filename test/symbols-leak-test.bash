97f41fe4ac034d62551967e785f6705a10042435
Ran Benita <ran234@gmail.com>
2019-07-25 13:24:00 +0300

    test/symbols-leak-test: make it work with macOS diff
    The <() stuff fails with an error:
        diff: extra operand `/dev/fd/61'
    
    Signed-off-by: Ran Benita <ran234@gmail.com>

--- test/symbols-leak-test.bash
+++ test/symbols-leak-test.bash
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
-set -e
+set -o pipefail -o errexit -o nounset
+
+tempdir=$(mktemp -d "$top_builddir"/symbols-leak-test.XXXXXXXXXX)
+trap 'rm -rf "$tempdir"' EXIT
 
 # Check that all exported symbols are specified in the symbol version
 # scripts.  If this fails, please update the appropriate .map file
 # (adding new version nodes as needed).
 
 # xkbcommon symbols
-diff -a -u \
-    <(grep -h '^\s\+xkb_' "$top_srcdir"/xkbcommon.map | sed -e 's/^\s\+\(.*\);/\1/' | sort) \
-    <(grep -h 'XKB_EXPORT' -A1 "$top_srcdir"/src/{,xkbcomp,compose}/*.c | grep '^xkb_' | sed -e 's/(.*//' | sort)
+grep -h '^\s\+xkb_' "$top_srcdir"/xkbcommon.map | sed -e 's/^\s\+\(.*\);/\1/' | sort > "$tempdir"/symbols
+grep -h 'XKB_EXPORT' -A1 "$top_srcdir"/src/{,xkbcomp,compose}/*.c | grep '^xkb_' | sed -e 's/(.*//' | sort > "$tempdir"/exported
+diff -a -u "$tempdir"/symbols "$tempdir"/exported
 
 # xkbcommon-x11 symbols
-diff -a -u \
-    <(grep -h '^\s\+xkb_.*' "$top_srcdir"/xkbcommon-x11.map | sed -e 's/^\s\+\(.*\);/\1/' | sort) \
-    <(grep -h 'XKB_EXPORT' -A1 "$top_srcdir"/src/x11/*.c | grep '^xkb_' | sed -e 's/(.*//' | sort)
+grep -h '^\s\+xkb_.*' "$top_srcdir"/xkbcommon-x11.map | sed -e 's/^\s\+\(.*\);/\1/' | sort > "$tempdir"/symbols
+grep -h 'XKB_EXPORT' -A1 "$top_srcdir"/src/x11/*.c | grep '^xkb_' | sed -e 's/(.*//' | sort > "$tempdir"/exported
+diff -a -u "$tempdir"/symbols "$tempdir"/exported
