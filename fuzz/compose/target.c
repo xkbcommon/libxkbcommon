5a063ce0a419d71a90b3eaef92a3c969e61942f3
M Hickford <mirth.hickford@gmail.com>
2022-02-05 20:31:08 +0100

    Attempt to follow https://google.github.io/clusterfuzzlite/build-integration/
        ~/oss-fuzz/infra/helper.py build_fuzzers --external ~/libxkbcommon
    
    Not yet working

--- fuzz/compose/target.c
+++ fuzz/compose/target.c
@@ -10,37 +10,16 @@
 #include "xkbcommon/xkbcommon.h"
 #include "xkbcommon/xkbcommon-compose.h"
 
-int
-main(int argc, char *argv[])
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     struct xkb_context *ctx;
-    FILE *file;
     struct xkb_compose_table *table;
-
-    if (argc != 2) {
-        fprintf(stderr, "usage: %s <file>\n", argv[0]);
-        return 1;
-    }
-
-    ctx = xkb_context_new(XKB_CONTEXT_NO_DEFAULT_INCLUDES | XKB_CONTEXT_NO_ENVIRONMENT_NAMES);
+    ctx = xkb_context_new(XKB_CONTEXT_NO_DEFAULT_INCLUDES); // | XKB_CONTEXT_NO_ENVIRONMENT_NAMES);
     assert(ctx);
-
-#ifdef __AFL_HAVE_MANUAL_CONTROL
-  __AFL_INIT();
-
-    while (__AFL_LOOP(1000))
-#endif
-    {
-        file = fopen(argv[1], "rb");
-        assert(file);
-        table = xkb_compose_table_new_from_file(ctx, file,
-                                                "en_US.UTF-8",
-                                                XKB_COMPOSE_FORMAT_TEXT_V1,
-                                                XKB_COMPOSE_COMPILE_NO_FLAGS);
-        xkb_compose_table_unref(table);
-        fclose(file);
-    }
-
-    puts(table ? "OK" : "FAIL");
+    table = xkb_compose_table_new_from_buffer(ctx, (const char*) data, size, "en_US.UTF-8",
+                                      XKB_COMPOSE_FORMAT_TEXT_V1,
+                                      XKB_COMPOSE_COMPILE_NO_FLAGS);
+    xkb_compose_table_unref(table);
     xkb_context_unref(ctx);
+    return 0;
 }
