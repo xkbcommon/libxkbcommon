49f8e601dea40fd70ad59c5af2cc3dd7c2eac4e0
Pierre Le Marre <dev@wismill.eu>
2023-09-28 09:50:43 +0200

    Python: make ruff & black happy

--- test/symbols-leak-test.py
+++ test/symbols-leak-test.py
@@ -10,15 +10,15 @@ import re
 import sys
 
 
-top_srcdir = pathlib.Path(os.environ['top_srcdir'])
+top_srcdir = pathlib.Path(os.environ["top_srcdir"])
 
 
 def symbols_from_map(path):
-    return re.findall(r'^\s+(xkb_.*);', path.read_text('utf-8'), re.MULTILINE)
+    return re.findall(r"^\s+(xkb_.*);", path.read_text("utf-8"), re.MULTILINE)
 
 
 def symbols_from_src(path):
-    return re.findall(r'XKB_EXPORT.*\n(xkb_.*)\(', path.read_text('utf-8'))
+    return re.findall(r"XKB_EXPORT.*\n(xkb_.*)\(", path.read_text("utf-8"))
 
 
 def diff(map_path, src_paths):
@@ -31,32 +31,32 @@ exit = 0
 
 # xkbcommon symbols
 left, right = diff(
-    top_srcdir/'xkbcommon.map',
+    top_srcdir / "xkbcommon.map",
     [
-        *(top_srcdir/'src').glob('*.c'),
-        *(top_srcdir/'src'/'xkbcomp').glob('*.c'),
-        *(top_srcdir/'src'/'compose').glob('*.c'),
+        *(top_srcdir / "src").glob("*.c"),
+        *(top_srcdir / "src" / "xkbcomp").glob("*.c"),
+        *(top_srcdir / "src" / "compose").glob("*.c"),
     ],
 )
 if left:
-    print('xkbcommon map has extra symbols:', ' '.join(left))
+    print("xkbcommon map has extra symbols:", " ".join(left))
     exit = 1
 if right:
-    print('xkbcommon src has extra symbols:', ' '.join(right))
+    print("xkbcommon src has extra symbols:", " ".join(right))
     exit = 1
 
 # xkbcommon-x11 symbols
 left, right = diff(
-    top_srcdir/'xkbcommon-x11.map',
+    top_srcdir / "xkbcommon-x11.map",
     [
-        *(top_srcdir/'src'/'x11').glob('*.c'),
+        *(top_srcdir / "src" / "x11").glob("*.c"),
     ],
 )
 if left:
-    print('xkbcommon-x11 map has extra symbols:', ' '.join(left))
+    print("xkbcommon-x11 map has extra symbols:", " ".join(left))
     exit = 1
 if right:
-    print('xkbcommon-x11 src has extra symbols:', ' '.join(right))
+    print("xkbcommon-x11 src has extra symbols:", " ".join(right))
     exit = 1
 
 sys.exit(exit)
