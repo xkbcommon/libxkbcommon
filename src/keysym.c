816379961176af0e9e58545a0ddb7cb50eb64daf
Pierre Le Marre <dev@wismill.eu>
2023-12-05 17:51:46 +0100

    keysyms: Fix xkb_keysym_is_modifier
    Currently `xkb_keysym_is_modifier` does not detect the following keysyms:
    - `XKB_KEY_ISO_Level5_Shift`
    - `XKB_KEY_ISO_Level5_Latch`
    - `XKB_KEY_ISO_Level5_Lock`
    
    Indeed, there is a mistake in the keysym interval that the code checks.
    The reason seems a confusing order of the keysyms in
    `xkbcommon-keysyms.h`: the current code has a comment “libX11 only goes
    up to XKB_KEY_ISO_Level5_Lock”, but in fact the modifiers keysyms are
    listed in a _semantic_ order in `xkbcommon-keysyms.h`, not in the
    increasing keysym _value_ order.
    
    Fixed by using the same (correct) code as libX11 and added some tests.

--- src/keysym.c
+++ src/keysym.c
@@ -370,8 +370,7 @@ xkb_keysym_is_modifier(xkb_keysym_t keysym)
 {
     return
         (keysym >= XKB_KEY_Shift_L && keysym <= XKB_KEY_Hyper_R) ||
-        /* libX11 only goes upto XKB_KEY_ISO_Level5_Lock. */
-        (keysym >= XKB_KEY_ISO_Lock && keysym <= XKB_KEY_ISO_Last_Group_Lock) ||
+        (keysym >= XKB_KEY_ISO_Lock && keysym <= XKB_KEY_ISO_Level5_Lock) ||
         keysym == XKB_KEY_Mode_switch ||
         keysym == XKB_KEY_Num_Lock;
 }
