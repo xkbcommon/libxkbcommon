8cca3a7bfb185876994d6ec3d25cda88e5640a4d
Pierre Le Marre <dev@wismill.eu>
2023-12-05 17:39:59 +0100

    compose: Add XKB_COMPOSE_MAX_STRING_SIZE
    Define the maximum size of a compose sequence result string explicit as
    a constant and use it everywhere to improve the code readability.

--- tools/tools-common.c
+++ tools/tools-common.c
@@ -51,6 +51,7 @@
 #include "tools-common.h"
 #include "src/utils.h"
 #include "src/keysym.h"
+#include "src/compose/parser.h"
 
 static void
 print_keycode(struct xkb_keymap *keymap, const char* prefix,
@@ -157,9 +158,7 @@ tools_print_keycode_state(char *prefix,
     xkb_keysym_t sym;
     const xkb_keysym_t *syms;
     int nsyms;
-    // FIXME: this buffer is used for xkb_compose_state_get_utf8,
-    // which can have a length up to 256. Need to import this constant from compose.
-    char s[MAX(16, XKB_KEYSYM_NAME_MAX_SIZE)];
+    char s[MAX(XKB_COMPOSE_MAX_STRING_SIZE, XKB_KEYSYM_NAME_MAX_SIZE)];
     xkb_layout_index_t layout;
     enum xkb_compose_status status;
 
