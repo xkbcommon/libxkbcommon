c1e2cc2c62c8672ee94d8705864254f5fe83c0e9
Pierre Le Marre <dev@wismill.eu>
2024-09-12 16:52:40 +0200

    Add UTF-8 to UTF-32 decoding
    Add internal functions to convert UTF-32 to UTF-8, with corresponding
    tests.

---
+++ src/utf8-decoding.h
@@ -0,0 +1,20 @@
+
+#ifndef UTF8_DECODING_H
+#define UTF8_DECODING_H
+
+#include "config.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* Check if a char is the start of a UTF-8 sequence */
+#define is_utf8_start(c) (((c) & 0xc0) != 0x80)
+#define INVALID_UTF8_CODE_POINT UINT32_MAX
+
+uint8_t
+utf8_sequence_length(const char *s);
+
+uint32_t
+utf8_next_code_point(const char *s, size_t max_size, size_t *size_out);
+
+#endif
