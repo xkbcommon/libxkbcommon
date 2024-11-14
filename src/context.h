f95bddeee0bfc8d38e8ba0ebf96054696bc8a64b
Pierre Le Marre <dev@wismill.eu>
2024-11-14 17:41:45 +0100

    Make context ref counting thread-safe

--- src/context.h
+++ src/context.h
@@ -26,11 +26,14 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include <stdatomic.h>
+
+#include "xkbcommon/xkbcommon.h"
 #include "atom.h"
 #include "messages-codes.h"
 
 struct xkb_context {
-    int refcnt;
+    atomic_int refcnt;
 
     ATTR_PRINTF(3, 0) void (*log_fn)(struct xkb_context *ctx,
                                      enum xkb_log_level level,
