f01f0d638ee5fbfee82e8da50d0ad3fc4e2a22fe
Pierre Le Marre <dev@wismill.eu>
2025-03-14 16:48:56 +0100

    test: Check more tools options combinations

--- test/tool-option-parsing.py
+++ test/tool-option-parsing.py
@@ -12,6 +12,10 @@ import subprocess
 import sys
 import tempfile
 import unittest
+from abc import ABCMeta, abstractmethod
+from collections.abc import Generator
+from dataclasses import dataclass
+from pathlib import Path
 
 try:
     top_builddir = os.environ["top_builddir"]
@@ -35,6 +39,18 @@ except KeyError:
 
 TIMEOUT = 10.0  # seconds
 
+# Unset some environment variables, so that testing --enable-environment-names
+# does not actually depend on the current environment.
+for key in (
+    "XKB_DEFAULT_RULES",
+    "XKB_DEFAULT_MODEL",
+    "XKB_DEFAULT_LAYOUT",
+    "XKB_DEFAULT_VARIANT",
+    "XKB_DEFAULT_OPTIONS",
+):
+    if key in os.environ:
+        del os.environ[key]
+
 # Ensure locale is C, so we can check error messages in English
 os.environ["LC_ALL"] = "C.UTF-8"
 
@@ -42,20 +58,118 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger("test")
 logger.setLevel(logging.DEBUG)
 
-# Permutation of RMLVO that we use in multiple tests
-rmlvos = [
-    list(x)
-    for x in itertools.permutations(
-        ["--rules=evdev", "--model=pc104", "--layout=ch", "--options=eurosign:5"]
+
+def powerset(iterable):
+    "Subsequences of the iterable from shortest to longest."
+    # powerset([1,2,3]) → () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+    s = tuple(iterable)
+    return itertools.chain.from_iterable(
+        itertools.combinations(s, r) for r in range(len(s) + 1)
     )
-]
+
+
+# Permutation of RMLVO that we use in multiple tests
+rmlvo_options = (
+    "--rules=evdev",
+    "--model=pc104",
+    "--layout=ch",
+    "--options=eurosign:5",
+    "--enable-environment-names",
+)
+rmlvos = tuple(
+    map(
+        list,
+        itertools.chain(
+            itertools.permutations(rmlvo_options[:-1]),
+            powerset(rmlvo_options),
+        ),
+    )
+)
+
+
+@dataclass
+class RMLVO:
+    rules: str | None = None
+    model: str | None = None
+    layout: str | None = None
+    variant: str | None = None
+    options: str | None = None
+    env: bool = False
+
+    def __iter__(self) -> Generator[tuple[str, ...]]:
+        if self.rules is not None:
+            yield "--rules", self.rules
+        if self.model is not None:
+            yield "--model", self.model
+        if self.layout is not None:
+            yield "--layout", self.layout
+        if self.variant is not None:
+            yield "--variant", self.variant
+        if self.options is not None:
+            yield "--options", self.options
+        if self.env:
+            yield ("--enable-environment-names",)
+
+    @property
+    def args(self) -> list[str]:
+        return list(itertools.chain.from_iterable(self))
+
+
+class Target(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def args(self) -> list[str]: ...
+
+    @property
+    def stdin(self) -> str | None:
+        return None
+
+
+class RmlvoTarget(Target):
+    @property
+    def args(self) -> list[str]:
+        return ["--rmlvo"]
+
+
+@dataclass
+class KeymapTarget(Target, RMLVO):
+    arg: bool = False
+    path: Path | None = None
+    stdin: str | None = None
+
+    def _args(self) -> Generator[str]:
+        yield from itertools.chain.from_iterable(self)
+        if self.arg:
+            yield "--keymap"
+        if self.path:
+            yield str(self.path)
+
+    @property
+    def args(self) -> list[str]:
+        return list(self._args())
+
+    @property
+    def use_rmlvo(self) -> bool:
+        return (
+            not self.arg
+            and not self.path
+            and (
+                bool(self.rules)
+                or bool(self.model)
+                or bool(self.layout)
+                or bool(self.variant)
+                or bool(self.options)
+                or bool(self.env)
+                or not self.stdin
+            )
+        )
 
 
 def _disable_coredump():
     resource.setrlimit(resource.RLIMIT_CORE, (0, 0))
 
 
-def run_command(args):
+def run_command(args, input: str | None = None):
     logger.debug("run command: {}".format(" ".join(args)))
 
     try:
@@ -63,6 +177,7 @@ def run_command(args):
             args,
             preexec_fn=_disable_coredump,
             capture_output=True,
+            input=input,
             text=True,
             timeout=0.7,
         )
@@ -81,7 +196,7 @@ class XkbcliTool:
         self.skipIf = skipIf
         self.skipError = skipError
 
-    def run_command(self, args):
+    def run_command(self, args, input: str | None = None):
         for condition, reason in self.skipIf:
             if condition:
                 raise unittest.SkipTest(reason)
@@ -91,10 +206,10 @@ class XkbcliTool:
             tool = self.xkbcli_tool
         args = [os.path.join(self.tool_path, tool)] + args
 
-        return run_command(args)
+        return run_command(args, input=input)
 
-    def run_command_success(self, args):
-        rc, stdout, stderr = self.run_command(args)
+    def run_command_success(self, args, input: str | None = None):
+        rc, stdout, stderr = self.run_command(args, input=input)
         if rc != 0:
             for testfunc, reason in self.skipError:
                 if testfunc(rc, stdout, stderr):
@@ -102,8 +217,8 @@ class XkbcliTool:
         assert rc == 0, (rc, stdout, stderr)
         return stdout, stderr
 
-    def run_command_invalid(self, args):
-        rc, stdout, stderr = self.run_command(args)
+    def run_command_invalid(self, args, input: str | None = None):
+        rc, stdout, stderr = self.run_command(args, input=input)
         assert rc == 2, (rc, stdout, stderr)
         return rc, stdout, stderr
 
@@ -214,25 +329,61 @@ class TestXkbcli(unittest.TestCase):
         self.xkbcli.run_command_invalid(["a"] * 64)
 
     def test_compile_keymap_args(self):
-        for args in (
-            ["--verbose"],
-            ["--rmlvo"],
-            # ['--kccgst'],
-            ["--verbose", "--rmlvo"],
-            # ['--verbose', '--kccgst'],
+        xkb_root = Path(os.environ["XKB_CONFIG_ROOT"])
+        keymap_path = xkb_root / "keymaps/masks.xkb"
+        keymap_string = keymap_path.read_text(encoding="utf-8")
+        test_option = "--test"
+        target: Target
+        for target in (
+            RmlvoTarget(),
+            # TODO: --kccgst
+            # Keymap from RMLVO
+            KeymapTarget(),
+            # Keymap from RMLVO (stdin ignored)
+            KeymapTarget(layout="us", stdin=keymap_string),
+            KeymapTarget(env=True, stdin=keymap_string),
+            # Keymap parsed from keymap file
+            KeymapTarget(arg=False, path=keymap_path),
+            KeymapTarget(arg=True, path=keymap_path),
+            # Keymap parsed from stdin
+            KeymapTarget(arg=False, stdin=keymap_string),
+            KeymapTarget(arg=True, stdin=keymap_string),
         ):
-            with self.subTest(args=args):
-                self.xkbcli_compile_keymap.run_command_success(args)
+            for options in powerset(("--verbose", test_option)):
+                args = list(options) + target.args
+                with self.subTest(args=args):
+                    stdout, stderr = self.xkbcli_compile_keymap.run_command_success(
+                        args, input=target.stdin
+                    )
+                    if test_option in options:
+                        # Expect no output
+                        assert stdout == "", (target, stdout)
+                    if isinstance(target, KeymapTarget) and test_option not in options:
+                        # Check keymap output for excepted bits:
+                        # - <AB01> is not defined in mask.xkb
+                        # - virtual_modifiers Test01 is only defined in masks.xkb
+                        assert ("<AB01>" in stdout) ^ (not target.use_rmlvo), (
+                            target,
+                            stdout,
+                            stderr,
+                        )
+                        assert ("virtual_modifiers Test01" in stdout) ^ (
+                            target.use_rmlvo
+                        ), (target, stdout, stderr)
 
     def test_compile_keymap_rmlvo(self):
-        def run(rmlvo):
-            return self.xkbcli_compile_keymap.run_command_success(rmlvo)
+        def run(target, rmlvo):
+            return self.xkbcli_compile_keymap.run_command_success(target + rmlvo)
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = {executor.submit(run, rmlvo): rmlvo for rmlvo in rmlvos}
+            futures = {
+                executor.submit(run, target.args, rmlvo): (target, rmlvo)
+                for target in (RmlvoTarget(), KeymapTarget())
+                for rmlvo in rmlvos
+            }
             for future in concurrent.futures.as_completed(futures, TIMEOUT):
-                rmlvo = futures[future]
-                with self.subTest(rmlvo=rmlvo):
+                target, rmlvo = futures[future]
+                with self.subTest(target=target, rmlvo=rmlvo):
                     future.result()
 
     def test_compile_keymap_include(self):
