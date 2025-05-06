a53daedba6dd92be3abbad1d708d3edac65e1da7
Pierre Le Marre <dev@wismill.eu>
2025-05-06 18:05:06 +0200

    Add xkb_keymap_new_from_names2
    This is just `xkb_keymap_new_from_names()` with an explicit keymap
    format.

--- src/keymap.c
+++ src/keymap.c
@@ -102,13 +102,13 @@ get_keymap_format_ops(enum xkb_keymap_format format)
 }
 
 struct xkb_keymap *
-xkb_keymap_new_from_names(struct xkb_context *ctx,
-                          const struct xkb_rule_names *rmlvo_in,
-                          enum xkb_keymap_compile_flags flags)
+xkb_keymap_new_from_names2(struct xkb_context *ctx,
+                           const struct xkb_rule_names *rmlvo_in,
+                           enum xkb_keymap_format format,
+                           enum xkb_keymap_compile_flags flags)
 {
     struct xkb_keymap *keymap;
     struct xkb_rule_names rmlvo;
-    const enum xkb_keymap_format format = XKB_KEYMAP_FORMAT_TEXT_V1;
     const struct xkb_keymap_format_ops *ops;
 
     ops = get_keymap_format_ops(format);
@@ -140,6 +140,16 @@ xkb_keymap_new_from_names(struct xkb_context *ctx,
     }
 
     return keymap;
+}
+
+
+struct xkb_keymap *
+xkb_keymap_new_from_names(struct xkb_context *ctx,
+                          const struct xkb_rule_names *rmlvo_in,
+                          enum xkb_keymap_compile_flags flags)
+{
+    return xkb_keymap_new_from_names2(ctx, rmlvo_in,
+                                      XKB_KEYMAP_FORMAT_TEXT_V1, flags);
 }
 
 struct xkb_keymap *
