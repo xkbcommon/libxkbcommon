51c8c5b5e4e58f0ab43a05ce17c21558c87a1eca
Pierre Le Marre <dev@wismill.eu>
2025-05-09 00:04:42 +0200

    doc: Common modifiers & LEDs names

--- include/xkbcommon/xkbcommon-names.h
+++ include/xkbcommon/xkbcommon-names.h
@@ -13,7 +13,21 @@
  * @brief Predefined names for common modifiers and LEDs.
  */
 
-/* *Real* modifiers names are hardcoded in libxkbcommon */
+/**
+ * @defgroup modifier-names Predefined names for common modifiers and LEDs
+ *
+ * @{
+ */
+
+/**
+ * @defgroup real-modifier-names Real modifiers names
+ *
+ * [*Real* modifiers][real modifier] names are hardcoded in libxkbcommon.
+ *
+ * [real modifier]: @ref real-modifier-def
+ *
+ * @{
+ */
 #define XKB_MOD_NAME_SHIFT      "Shift"
 #define XKB_MOD_NAME_CAPS       "Lock"
 #define XKB_MOD_NAME_CTRL       "Control"
@@ -22,31 +36,98 @@
 #define XKB_MOD_NAME_MOD3       "Mod3"
 #define XKB_MOD_NAME_MOD4       "Mod4"
 #define XKB_MOD_NAME_MOD5       "Mod5"
+/** @} */
 
-/* Usual virtual modifiers mappings to real modifiers */
-#define XKB_MOD_NAME_ALT        "Mod1" /* Alt */
-#define XKB_MOD_NAME_LOGO       "Mod4" /* Super */
-#define XKB_MOD_NAME_NUM        "Mod2" /* NumLock */
-
-/* Common *virtual* modifiers, encoded in xkeyboard-config in the compat and
- * symbols files. They have been stable since the beginning of the project and
- * are unlikely to ever change. */
+/**
+ * @defgroup virtual-modifier-names Virtual modifiers names
+ *
+ * Common [*virtual* modifiers][virtual modifiers], encoded in [xkeyboard-config]
+ * in the [compat] and [symbols] files. They have been stable since the beginning
+ * of the project and are unlikely to ever change.
+ *
+ * [virtual modifiers]: @ref virtual-modifier-def
+ * [xkeyboard-config]: https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config
+ * [compat]: @ref the-xkb_compat-section
+ * [symbols]: @ref the-xkb_symbols-section
+ *
+ * @{
+ */
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_ALT       "Alt"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_HYPER     "Hyper"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_LEVEL3    "LevelThree"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_LEVEL5    "LevelFive"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_META      "Meta"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_NUM       "NumLock"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_SCROLL    "ScrollLock"
+/** @since 1.8.0 */
 #define XKB_VMOD_NAME_SUPER     "Super"
+/** @} */
 
-/* LEDs names are encoded in xkeyboard-config, in the keycodes and compat files.
- * They have been stable since the beginning of the project and are unlikely to
- * ever change. */
+/**
+ * @defgroup legacy-virtual-modifier-names Legacy virtual modifier names
+ *
+ * Usual [*virtual* modifiers][virtual modifiers] mappings to
+ * [*real* modifiers][real modifiers].
+ *
+ * [virtual modifiers]: @ref virtual-modifier-def
+ * [real modifiers]: @ref real-modifier-def
+ *
+ * @{
+ */
+/**
+ * Usual [*real* modifier][real modifier] for the
+ * [*virtual* modifier][virtual modifier] `Alt`.
+ *
+ * [virtual modifier]: @ref virtual-modifier-def
+ * [real modifier]: @ref real-modifier-def
+ */
+#define XKB_MOD_NAME_ALT        "Mod1"
+/**
+ * Usual [*real* modifier][real modifier] for the
+ * [*virtual* modifier][virtual modifier] `Super`.
+ *
+ * [virtual modifier]: @ref virtual-modifier-def
+ * [real modifier]: @ref real-modifier-def
+ */
+#define XKB_MOD_NAME_LOGO       "Mod4"
+/**
+ * Usual [*real* modifier][real modifier] for the
+ * [*virtual* modifier][virtual modifier] `NumLock`.
+ *
+ * [virtual modifier]: @ref virtual-modifier-def
+ * [real modifier]: @ref real-modifier-def
+ */
+#define XKB_MOD_NAME_NUM        "Mod2"
+/** @} */
+
+/**
+ * @defgroup led-names LEDs names
+ *
+ * LEDs names are encoded in [xkeyboard-config], in the [keycodes] and [compat]
+ * files. They have been stable since the beginning of the project and are
+ * unlikely to ever change.
+ *
+ * [xkeyboard-config]: https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config
+ * [keycodes]: @ref the-xkb_keycodes-section
+ * [compat]: @ref the-xkb_compat-section
+ *
+ * @{
+ */
 #define XKB_LED_NAME_NUM        "Num Lock"
 #define XKB_LED_NAME_CAPS       "Caps Lock"
 #define XKB_LED_NAME_SCROLL     "Scroll Lock"
+/** @since 1.8.0 */
 #define XKB_LED_NAME_COMPOSE    "Compose"
 #define XKB_LED_NAME_KANA       "Kana"
+/** @} */
+
+/** @} */
 
 #endif
