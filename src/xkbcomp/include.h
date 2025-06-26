550e36c14be8ab41f39b55ee7f5ef65d6d37c832
Pierre Le Marre <dev@wismill.eu>
2025-06-26 16:58:50 +0200

    include: Fix infinite loop
    Fixed including an absolute path with no default map triggering an
    infinite loop.

--- src/xkbcomp/include.h
+++ src/xkbcomp/include.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include "ast.h"
+#include "utils.h"
 
 /* Reasonable threshold, with plenty of margin for keymaps in the wild */
 #define INCLUDE_MAX_DEPTH 15
@@ -19,9 +20,15 @@
 static const char MERGE_MODE_PREFIXES[] =
     { MERGE_OVERRIDE_PREFIX, MERGE_AUGMENT_PREFIX, MERGE_REPLACE_PREFIX, 0 };
 
+
 bool
 ParseIncludeMap(char **str_inout, char **file_rtrn, char **map_rtrn,
                 char *nextop_rtrn, char **extra_data);
+
+ssize_t
+expand_path(struct xkb_context *ctx, const char* parent_file_name,
+            const char *name, size_t name_len, enum xkb_file_type type,
+            char *buf, size_t buf_size);
 
 FILE *
 FindFileInXkbPath(struct xkb_context *ctx, const char* parent_file_name,
@@ -32,5 +39,5 @@ bool
 ExceedsIncludeMaxDepth(struct xkb_context *ctx, unsigned int include_depth);
 
 XkbFile *
-ProcessIncludeFile(struct xkb_context *ctx, IncludeStmt *stmt,
+ProcessIncludeFile(struct xkb_context *ctx, const IncludeStmt *stmt,
                    enum xkb_file_type file_type);
