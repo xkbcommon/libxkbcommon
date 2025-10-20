xkbcommon/libxkbcommon

commit 135b3204a15838205c97e793cd41faa742d429e2

compose: Add fallback for custom locales
Before this commit, loading a Compose file based on the locale would fail
if the locale is not in the X11 Compose locale registry. While there are
workarounds (e.g. `~/.XCompose` file or `$XCOMPOSEFILE`), it does not
work if the corresponding file has `include "%L"`.

This commit adds the fallback `en_US.UTF-8` in case the locale is
installed but not registered in the X11 Compose locale registry. The
choice is motivated by the fact that most locales use `en_US.UTF-8`
anyway.

Ideally we should have a mechanism to extend the Compose locale registry
at the *system* level. Mechanisms at the user level (e.g. custom Compose
file, environment variable) are deemed sufficient.

We could still improve it by first trying to fallback to the locale
without the country bits, but there is no function to do such function
in the stdlib and we do not want to mess with locales manually.

Unfortunately is not possible to test it in our test suite. One can
still check it works following these instructions:

1. Create a custom locale, e.g. `en_XX.UTF-8`. `glibc-i18ndata` or
   similar package may be required to get the required files in
   `/usr/share/i18n/`.

   `sudo localedef -i en_US -f UTF-8 en_XX.UTF-8`
2. `xkbcli compile-compose --verbose --locale en_XX.UTF-8`

include/xkbcommon/xkbcommon-compose.h
@@ -196,6 +196,9 @@ enum xkb_compose_format {
  *    X locale files, e.g. `/usr/share/X11/locale`, instead of the
  *    preconfigured directory.
  *
+ * Since 1.12, system locales not registered in `$XLOCALEDIR` will fallback
+ * to `en_US.UTF-8`.
+ *
  * @param context
  *     The library context in which to create the compose table.
  * @param locale

meson.build
@@ -153,6 +153,9 @@ if not cc.has_header_symbol('limits.h', 'PATH_MAX', prefix: system_ext_define)
         configh_data.set('PATH_MAX', 4096)
     endif
 endif
+if cc.has_header_symbol('locale.h', 'newlocale', prefix: system_ext_define)
+    configh_data.set('HAVE_NEWLOCALE', 1)
+endif
 
 # Silence some security & deprecation warnings on MSVC
 # for some unix/C functions we use.

.github/workflows/linux.yml
@@ -58,6 +58,10 @@ jobs:
           meson install -C $BUILDDIR
           popd
           popd
+      - name: Create custom locale for testing
+        run: |
+          # The locale name must be kept in sync with the Compose test
+          sudo localedef -i en_US -f UTF-8 xx_YY.UTF-8
       - name: Setup
         run: |
           # -gdwarf-4 - see https://github.com/llvm/llvm-project/issues/56550.

src/compose/paths.c
@@ -5,7 +5,10 @@
 
 #include "config.h"
 
+#include <locale.h>
+
 #include "xkbcommon/xkbcommon.h"
+#include "messages-codes.h"
 #include "utils.h"
 #include "utils-paths.h"
 #include "context.h"
@@ -179,12 +182,49 @@ get_locale_compose_file_path(struct xkb_context *ctx, const char *locale)
      * is UTF-8 based, and since 99% of the time a C locale is really just
      * a misconfiguration for UTF-8, let's do the most helpful thing.
      */
+    static const char * const fallback = "en_US.UTF-8";
+    static const char * const registry = "compose.dir";
     if (streq(locale, "C"))
-        locale = "en_US.UTF-8";
+        locale = fallback;
 
-    resolved = resolve_name(ctx, "compose.dir", RIGHT_TO_LEFT, locale);
-    if (!resolved)
+    resolved = resolve_name(ctx, registry, RIGHT_TO_LEFT, locale);
+    if (!resolved) {
+#ifdef HAVE_NEWLOCALE
+        /*
+         * There is no extension mechanism for X11 Compose locales.
+         * Instead of failing, we just use `en_US.UTF-8` as a fallback because
+         * it is what most locales use anyway.
+         * But we still want to fail if the locale is invalid in the system,
+         * so that we can catch missing system locales and typos.
+         */
+        locale_t loc = newlocale(LC_ALL_MASK, locale, (locale_t) 0);
+        if (loc == (locale_t) 0) {
+            log_err(ctx, XKB_ERROR_INVALID_COMPOSE_LOCALE,
+                    "No Compose file for locale \"%s\": "
+                    "locale is either invalid or not installed\n", locale);
+            return NULL;
+        } else {
+            /*
+             * The locale is legit but have no entry in the X11 Compose registry,
+             * so use the `en_US.UTF-8` fallback.
+             */
+            freelocale(loc);
+            resolved = resolve_name(ctx, registry, RIGHT_TO_LEFT, fallback);
+            if (!resolved) {
+                log_err(ctx, XKB_ERROR_INVALID_COMPOSE_LOCALE,
+                        "No Compose file for locale \"%s\": "
+                        "failed to use fallback \"%s\"\n", locale, fallback);
+                return NULL;
+            } else {
+                log_warn(ctx, XKB_ERROR_INVALID_COMPOSE_LOCALE,
+                         "No Compose file for locale \"%s\": "
+                         "using locale fallback \"%s\"\n", locale, fallback);
+            }
+        }
+#else
         return NULL;
+#endif
+    }
 
     if (is_absolute_path(resolved)) {
         path = resolved;

test/compose.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <errno.h>
+#include <locale.h>
 #include <stdio.h>
 
 #include "xkbcommon/xkbcommon-compose.h"
@@ -577,6 +578,24 @@ test_from_locale(struct xkb_context *ctx)
                                               XKB_COMPOSE_COMPILE_NO_FLAGS);
     assert(table);
     xkb_compose_table_unref(table);
+
+#ifdef HAVE_NEWLOCALE
+    /* Test custom locale. Require installing it system-wide */
+    /* NOTE: Keep the locale name in sync with the localedef call in our CI */
+    static const char * const custom_locale = "xx_YY.UTF-8";
+    locale_t loc = newlocale(LC_ALL, custom_locale, (locale_t) 0);
+    table = xkb_compose_table_new_from_locale(ctx, custom_locale,
+                                              XKB_COMPOSE_COMPILE_NO_FLAGS);
+    if (loc == (locale_t) 0) {
+        /* Locale is not installed: no fallback */
+        assert(!table);
+    } else {
+        /* Locale is installed */
+        freelocale(loc);
+        assert(table);
+        xkb_compose_table_unref(table);
+    }
+#endif
 
     /* Bogus - not found. */
     table = xkb_compose_table_new_from_locale(ctx, "blabla",

commit 76085bd159a21cd2ede8633f96740250908349b4

compose: No fallback if no missing locale detection
Do not use `newlocale()` if it accepts missing locales. It happens with
muslc, while glibc works as expected.

It fixes the incorrect behavior of the fallback introduced in
135b3204a15838205c97e793cd41faa742d429e2.

include/xkbcommon/xkbcommon-compose.h
@@ -197,7 +197,8 @@ enum xkb_compose_format {
  *    preconfigured directory.
  *
  * Since 1.12, system locales not registered in `$XLOCALEDIR` will fallback
- * to `en_US.UTF-8`.
+ * to `en_US.UTF-8`, if missing locale detection is supported by the C standard
+ * library in use.
  *
  * @param context
  *     The library context in which to create the compose table.

meson.build
@@ -172,7 +172,20 @@ if not cc.has_header_symbol('limits.h', 'PATH_MAX', prefix: system_ext_define)
     endif
 endif
 if cc.has_header_symbol('locale.h', 'newlocale', prefix: system_ext_define)
-    configh_data.set('HAVE_NEWLOCALE', 1)
+    # Use newlocale only if it fails on missing locales
+    r = cc.run(
+        '''
+        #define _GNU_SOURCE
+        #include <locale.h>
+        int main(void){
+            locale_t loc = newlocale(LC_ALL_MASK, "¡NONSENSE!", (locale_t) 0);
+            return (loc != (locale_t) 0);
+        }''',
+        name: 'newlocale fails on invalid locales',
+    )
+    if r.compiled() and r.returncode() == 0
+        configh_data.set('HAVE_NEWLOCALE', 1)
+    endif
 endif
 
 # Silence some security & deprecation warnings on MSVC