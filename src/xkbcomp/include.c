xkbcommon/libxkbcommon

commit 9b0b8c6863a9f2f4d1b3e67ab23d16e12710936b

xkbcomp: Stricter handling of default map include
Before this commit, including a *default* map, i.e. without an explicit
section name (e.g. `include "au"` vs `include "au(basic)"`) would match
the first section of the first matching file in the XKB include paths,
even if this section is not an *explicit* default map (i.e. tagged with
`default`) but an *implicit* default map (i.e. the first map of the
file, i.e. a weak match).

It makes user configuration risky: say a user wants to create a custom
version `au(custom)` of the `au` layout:
- `./config/xkb/symbols/au`: custom layout in section “custom”.
- `/usr/share/X11/xkb/symbols/au`: system layout, with *default* section
  “basic”.

In this setup *any* layout that imports the default map from `au` would
in fact import the *implicit* default map `au(custom)` instead of the
*explicit* default map `au(basic)`.

This incorrect behavior may thus break setups with multiple layouts.
This is especially true for symbols files such as: `pc`, `us` or `latin`.

Fixed by trying harder to found the exact default map, defaulting to the
old behavior (weak match) only if no *explicit* default map (exact match)
has been found in the XKB include paths.

src/xkbcomp/include.c
@@ -259,11 +259,18 @@ XkbFile *
 ProcessIncludeFile(struct xkb_context *ctx, IncludeStmt *stmt,
                    enum xkb_file_type file_type)
 {
-    FILE *file;
-    XkbFile *xkb_file = NULL;
+    /*
+     * Resolve include statement:
+     * - Explicit map name: look for an *exact match* only.
+     * - Default map: look for an *explicit default* map (i.e. tagged with
+     *   `default`: exact match), else fallback to the first *implicit* default
+     *   map (weak match).
+     */
+    XkbFile *xkb_file = NULL;  /* Exact match */
+    XkbFile *candidate = NULL; /* Weak match */
     unsigned int offset = 0;
 
-    file = FindFileInXkbPath(ctx, stmt->file, file_type, NULL, &offset);
+    FILE* file = FindFileInXkbPath(ctx, stmt->file, file_type, NULL, &offset);
     if (!file)
         return NULL;
 
@@ -280,13 +287,40 @@ ProcessIncludeFile(struct xkb_context *ctx, IncludeStmt *stmt,
                         xkb_file_type_to_string(xkb_file->file_type), stmt->file);
                 FreeXkbFile(xkb_file);
                 xkb_file = NULL;
-            } else {
+            } else if (stmt->map || (xkb_file->flags && MAP_IS_DEFAULT)) {
+                /*
+                 * Exact match: explicit map name or explicit default map.
+                 * Lookup stops here.
+                 */
                 break;
+            } else if (!candidate) {
+                /*
+                 * Weak match: looking for an explicit default map, but found
+                 * only the first *implicit* default map (e.g. first map of the
+                 * file). Use as fallback, but keep looking for an exact match.
+                 */
+                candidate = xkb_file;
+                xkb_file = NULL;
+            } else {
+                /*
+                 * Weak match, but we already have a previous candidate.
+                 * Keep looking for an exact match.
+                 */
+                FreeXkbFile(xkb_file);
+                xkb_file = NULL;
             }
         }
 
         offset++;
         file = FindFileInXkbPath(ctx, stmt->file, file_type, NULL, &offset);
+    }
+
+    if (!xkb_file) {
+        /* No exact match: use weak match, if any */
+        xkb_file = candidate;
+    } else {
+        /* Found exact match: discard weak match, if any */
+        FreeXkbFile(candidate);
     }
 
     if (!xkb_file) {

test/data/extra/symbols/capslock
@@ -1,0 +1,3 @@
+xkb_symbols "custom" {
+	key <CAPS> { [ XF86Fn ] };
+};

test/buffercomp.c
@@ -261,7 +261,7 @@ test_bidi_chars(struct xkb_context *ctx, bool update_output_files)
 }
 
 static void
-test_recursive(struct xkb_context *ctx)
+test_recursive_includes(struct xkb_context *ctx)
 {
     struct xkb_keymap *keymap;
 
@@ -334,6 +334,72 @@ test_recursive(struct xkb_context *ctx)
         keymap = test_compile_buffer(ctx, keymaps[k], strlen(keymaps[k]));
         assert(!keymap);
     }
+}
+
+static void
+test_include_default_maps(bool update_output_files)
+{
+    struct xkb_context *ctx = xkb_context_new(XKB_CONTEXT_NO_DEFAULT_INCLUDES);
+    assert(ctx);
+    /* “User” config */
+    char *include_path = test_get_path("extra");
+    assert(include_path);
+    assert(xkb_context_include_path_append(ctx, include_path));
+    free(include_path);
+    /* “System” config */
+    include_path = test_get_path("");
+    assert(include_path);
+    assert(xkb_context_include_path_append(ctx, include_path));
+    free(include_path);
+
+    const struct keymap_test_data keymaps[] = {
+        {
+            /* Use system explicit default map */
+            .keymap =
+                "xkb_keymap {\n"
+                "  xkb_keycodes { <CAPS> = 66; };\n"
+                "  xkb_symbols { include \"capslock\" };\n"
+                "};",
+            .expected = GOLDEN_TESTS_OUTPUTS "include-capslock-system.xkb"
+        },
+        {
+            /* Use custom named map */
+            .keymap =
+                "xkb_keymap {\n"
+                "  xkb_keycodes { <CAPS> = 66; };\n"
+                "  xkb_symbols { include \"capslock(custom)\" };\n"
+                "};",
+            .expected = GOLDEN_TESTS_OUTPUTS "include-capslock-custom.xkb"
+        },
+        {
+            /* Use custom *explicit* default map */
+            .keymap =
+                "xkb_keymap {\n"
+                "  xkb_keycodes { <RALT> = 108; <LVL3> = 92; };\n"
+                "  xkb_types { include \"basic\" };\n"
+                "  xkb_symbols { include \"level3\" };\n"
+                "};",
+            .expected = GOLDEN_TESTS_OUTPUTS "include-level3-explicit-default.xkb"
+        },
+        {
+            /* Use custom *implicit* default map */
+            .keymap =
+                "xkb_keymap {\n"
+                "  xkb_keycodes { <> = 1; };\n"
+                "  xkb_symbols { include \"banana\" };\n"
+                "};",
+            .expected = GOLDEN_TESTS_OUTPUTS "include-banana-implicit-default.xkb"
+        },
+    };
+
+    for (unsigned int k = 0; k < ARRAY_SIZE(keymaps); k++) {
+        fprintf(stderr, "------\n*** %s: #%u ***\n", __func__, k);
+        assert(test_compile_output(ctx, compile_buffer, NULL, __func__,
+                                   keymaps[k].keymap, strlen(keymaps[k].keymap),
+                                   keymaps[k].expected, update_output_files));
+    }
+
+    xkb_context_unref(ctx);
 }
 
 /* Test some limits related to allocations */
@@ -1497,7 +1563,8 @@ main(int argc, char *argv[])
     test_component_syntax_error(ctx);
     test_optional_components(ctx, update_output_files);
     test_bidi_chars(ctx, update_output_files);
-    test_recursive(ctx);
+    test_recursive_includes(ctx);
+    test_include_default_maps(update_output_files);
     test_alloc_limits(ctx);
     test_integers(ctx, update_output_files);
     test_keycodes(ctx, update_output_files);

test/data/extra/symbols/banana
@@ -1,0 +1,7 @@
+xkb_symbols "1" {
+	key <> { ["🍉"] };
+};
+
+xkb_symbols "2" {
+	key <> { ["🍍"] };
+};

test/data/extra/symbols/level3
@@ -1,0 +1,5 @@
+default partial modifier_keys
+xkb_symbols "ralt_switch" {
+  key <RALT> { [ ISO_Level3_Shift, ISO_Level3_Lock ] };
+  include "level3(modifier_mapping)"
+};

test/data/keymaps/include-banana-implicit-default.xkb
@@ -1,0 +1,23 @@
+xkb_keymap {
+xkb_keycodes {
+	minimum = 1;
+	maximum = 255;
+	<>                   = 1;
+};
+
+xkb_types {
+	type "default" {
+		modifiers= none;
+	};
+};
+
+xkb_compatibility {
+	interpret.useModMapMods= AnyLevel;
+	interpret.repeat= False;
+};
+
+xkb_symbols "banana" {
+	key <>                   {	[          U1F349 ] };
+};
+
+};

test/data/keymaps/include-capslock-custom.xkb
@@ -1,0 +1,23 @@
+xkb_keymap {
+xkb_keycodes {
+	minimum = 8;
+	maximum = 255;
+	<CAPS>               = 66;
+};
+
+xkb_types {
+	type "default" {
+		modifiers= none;
+	};
+};
+
+xkb_compatibility {
+	interpret.useModMapMods= AnyLevel;
+	interpret.repeat= False;
+};
+
+xkb_symbols "capslock(custom)" {
+	key <CAPS>               {	[          XF86Fn ] };
+};
+
+};

test/data/keymaps/include-capslock-system.xkb
@@ -1,0 +1,24 @@
+xkb_keymap {
+xkb_keycodes {
+	minimum = 8;
+	maximum = 255;
+	<CAPS>               = 66;
+};
+
+xkb_types {
+	type "default" {
+		modifiers= none;
+	};
+};
+
+xkb_compatibility {
+	interpret.useModMapMods= AnyLevel;
+	interpret.repeat= False;
+};
+
+xkb_symbols "capslock" {
+	key <CAPS>               {	[       Caps_Lock ] };
+	modifier_map Lock { <CAPS> };
+};
+
+};

commit 0cc0c79ff3a592b5e731a1a4f6e3ff0091bea9f8

xkbcomp: Fix default section tag detection
Before this commit, detection of the `default` tag of XKB section
was broken: it wrongly matched the first section with at least one tag,
independently of the tag value. This resulted in possibly including the
wrong section when the include statement does not use an explicit
section name, e.g. `include "fr"` instead of `include "fr(basic)"`.

Thanksfully, xkeyboard-config has always the `default` section at the
top of the files, so only custom configurations could be affected.

- Fixed typo: `&&` → `&` introduced by
  9b0b8c6863a9f2f4d1b3e67ab23d16e12710936b.
- Added corresponding test.

src/xkbcomp/include.c
@@ -452,7 +452,7 @@ ProcessIncludeFile(struct xkb_context *ctx, const IncludeStmt *stmt,
                         xkb_file_type_to_string(xkb_file->file_type), stmt->file);
                 FreeXkbFile(xkb_file);
                 xkb_file = NULL;
-            } else if (stmt->map || (xkb_file->flags && MAP_IS_DEFAULT)) {
+            } else if (stmt->map || (xkb_file->flags & MAP_IS_DEFAULT)) {
                 /*
                  * Exact match: explicit map name or explicit default map.
                  * Lookup stops here.

test/data/extra/symbols/capslock
@@ -1,3 +1,3 @@
-xkb_symbols "custom" {
+partial xkb_symbols "custom" {
 	key <CAPS> { [ XF86Fn ] };
 };