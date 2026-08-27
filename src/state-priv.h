5fdebaf576d3bd83ced3e0f2adf5abe771ba8a56
Pierre Le Marre <dev@wismill.eu>
2026-08-27 15:12:02 +0200

    state: rename xkb_check_update_size
    Rename `xkb_check_update_size` to `xkb_check_state_abi`.

--- src/state-priv.h
+++ src/state-priv.h
@@ -292,11 +292,11 @@ static_assert(sizeof(struct xkb_machine_builder_shortcut_layout_update) * 30 <=
         sizeof(struct xkb_machine_builder_shortcut_layout_update)\
 )
 
-#define xkb_check_update_size(x) xkb_check_versioned_struct_size( \
-    xkb_versioned_struct_size_v1(x),                              \
-    xkb_versioned_struct_size_min(x),                             \
-    xkb_versioned_struct_reserved_offset(x),                      \
-    (x)                                                           \
+#define xkb_check_state_abi(x) xkb_check_versioned_struct_size( \
+    xkb_versioned_struct_size_v1(x),                            \
+    xkb_versioned_struct_size_min(x),                           \
+    xkb_versioned_struct_reserved_offset(x),                    \
+    (x)                                                         \
 )
 
 /* V1 is the smallest struct version */
